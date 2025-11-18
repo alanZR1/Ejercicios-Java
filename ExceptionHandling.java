e7fac8a8c59dbddd737f699ebb27f97b291651f3
Alan Reyes Luis <alannreyes02@gmail.com>
2025-11-18 15:42:47 -0600

    .

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -2,6 +2,7 @@ class ExceptionHandling {
     public static void main(String[] args) {
         try {
             int myInt = Integer.parseInt("123a"); // Esto arrojara NumberFormatException
+            System.out.println(myInt);
         } catch (NumberFormatException e) {
             System.out.println("Se ha producido una excepción: " + e.getMessage());
         }
